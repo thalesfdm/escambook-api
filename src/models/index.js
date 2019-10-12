b3ae168c5463980a635ff9782adbd9c50e6716d6
Thales F <thalesfdm@gmail.com>
2019-10-12 14:08:35 -0300

    Update index.js
    Fix: as relações estavam invertidas no hasOne.
    Coloquei em ordem também por nome do modelo.
    Os hasMany seguem comentados para vermos como vai funcionar.

--- src/models/index.js
+++ src/models/index.js
@@ -3,16 +3,16 @@ import sequelize from '../database/database';
 const models = {};
 
 models.Address = sequelize.import('./address.model');
-models.Book = sequelize.import('./book.model.js');
-models.Image = sequelize.import('./image.model.js');
+models.Book = sequelize.import('./book.model');
+models.Copy = sequelize.import('./copy.model');
+models.Image = sequelize.import('./image.model');
 models.User = sequelize.import('./user.model');
-//models.Copy = sequelize.import('./copy.model');
 
+models.Book.hasOne(models.Copy); // bookId
 models.Image.hasOne(models.User); // profilepic
 models.User.hasOne(models.Address);
+models.User.hasOne(models.Copy); // userId
 models.User.hasOne(models.Image); // uploaderid
-//models.Copy.hasOne(models.User); // userId
-//models.Copy.hasOne(models.Book); // bookId
 //models.User.hasMany(models.Copy);
 //models.Book.hasMany(models.Copy);
 
