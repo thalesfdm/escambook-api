0b5827f723d875b35416f98c2e4a832d757c1a8f
Thales F <thalesfdm@gmail.com>
2019-10-16 20:01:10 -0300

    Update user.controller.js
    Apenas alterei a ordem do delete para refletir a ordem das routes, e algumas alterações nas mensagens.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -122,55 +122,6 @@ class UserController {
 
   }
 
-  // @DELETE /api/users/books/:copyId
-  static async delCopy(req, res) {
-
-    const userId = req.user.userId;
-
-    if (!userId) {
-      return res.status(400).json({ success: false, message: 'invalid authentication' });
-    }
-
-    const { error } = Joi.validate(req.params,
-      {
-        copyId: Joi.number().integer().required(),
-      }
-    );
-
-    if (error) {
-      return res.status(400).json({ success: false, message: error.details[0].message });
-    }
-
-    const copyId = req.params.copyId;
-    const copy = await models.Copy.findOne({ where: { id: copyId, userId } });
-
-    if (!copy) {
-      return res.status(400).json({ success: false, message: 'copy not found' });
-    }
-
-    try {
-      await models.Copy.destroy({ where: { id: copyId } }); 
-
-      return res.json({ sucess: true, message: "sucessful copy removal", removed: copy});
-      
-    } catch (e) {
-
-      const error = {};
-
-      for (let i in e.errors) {
-        error[e.errors[i].validatorKey] = e.errors[i].message;
-      }
-
-      if (Object.keys(error).length > 0) {
-        return res.status(400).json({ success: false, message: error });
-      } else {
-        return res.status(400).json({ success: false, message: e.toString() });
-      }
-
-    }
-
-  }
-
   // @POST /api/users/login
   static async login(req, res) {
 
@@ -304,6 +255,55 @@ class UserController {
       return res.json({ success: true, message: 'registration successful', userId: user.id });
 
     } catch (e) {
+      const error = {};
+
+      for (let i in e.errors) {
+        error[e.errors[i].validatorKey] = e.errors[i].message;
+      }
+
+      if (Object.keys(error).length > 0) {
+        return res.status(400).json({ success: false, message: error });
+      } else {
+        return res.status(400).json({ success: false, message: e.toString() });
+      }
+
+    }
+
+  }
+  
+  // @DELETE /api/users/books/:copyId
+  static async delCopy(req, res) {
+
+    const userId = req.user.userId;
+
+    if (!userId) {
+      return res.status(400).json({ success: false, message: 'invalid authentication' });
+    }
+
+    const { error } = Joi.validate(req.params,
+      {
+        copyId: Joi.number().integer().required(),
+      }
+    );
+
+    if (error) {
+      return res.status(400).json({ success: false, message: error.details[0].message });
+    }
+
+    const copyId = req.params.copyId;
+    const copy = await models.Copy.findOne({ where: { id: copyId, userId } });
+
+    if (!copy) {
+      return res.status(400).json({ success: false, message: 'invalid copy or user' });
+    }
+
+    try {
+      await models.Copy.destroy({ where: { id: copyId } }); 
+
+      return res.json({ sucess: true, message: 'copy succesfully removed', removed: copy});
+      
+    } catch (e) {
+
       const error = {};
 
       for (let i in e.errors) {
